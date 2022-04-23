5d5ce7ce4692faa4020db19616bae4e4a319af5f
«Kwadyboo» <adeline.kinderf@gmail.com>
2022-04-23 14:06:02 +0200

    Added script file Change style and index

---
+++ script.js
@@ -0,0 +1,115 @@
+let scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+        // 1. Chiffre aléatoire / Random number
+
+        let dice = Math.floor(Math.random() * 6) + 1;
+
+        //2. Affiche le résultat / Display the result
+
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'img/dice-' + dice + '.svg';
+
+
+        //3. Met à jour le score du tour SI le numéro obtenu n'était PAS un 1 / Update the round score IF the rolled number was NOT a 1
+
+        if (dice !== 1) {
+
+            //Ajout du score / Add score
+
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+
+            //Joueur suivant / Next player
+
+            nextPlayer();
+        }
+    }    
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+
+        // Ajoute le score CURRENT au score GLOBAL / Add CURRENT score to GLOBAL score
+
+        scores[activePlayer] += roundScore;
+
+        // Met à jour l'interface utilisateur / Update the UI
+
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Vérifie si le joueur a gagné le jeu / Check if player won the game
+
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+
+            //Joueur suivant / Next player 
+
+            nextPlayer();
+        }
+    }
+});
+
+
+function nextPlayer() {
+
+    //Joueur suivant / Next player
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+// Affichage des règles dans une popover / Display of rulers in a popover
+
+$(function(){
+    $(".myPopover").popover();
+ });
+ 
+
+
+
+
+
+
